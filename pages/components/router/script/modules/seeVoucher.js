07ce53ffcde59481efcd32786e8026949508ebdf
yinjiazeng <yinjiazeng@163.com>
2017-07-26 20:35:23 +0800

    提交

--- pages/components/router/script/modules/seeVoucher.js
+++
@@ -1,6 +0,0 @@
-Nui.define(['../tpls/seeVoucher', 'template'], function(tmpl, tpl){
-    var module = this;
-    return function(target, wrapper, request){
-        wrapper.html(tpl.render(tmpl, request))
-    }
-})
