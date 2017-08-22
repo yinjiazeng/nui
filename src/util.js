1e9cf97389af74e9e8e47f29957857f9dfd08a74
yinjiazeng <yinjiazeng@163.com>
2017-08-22 12:24:52 +0800

    提交

--- src/util.js
+++ src/util.js
@@ -365,5 +365,175 @@ Nui.define('util', {
             text = document.getSelection().toString();
         }
         return !!text;
+    },
+    /**
+     * @func 检测是否需要安装PDF阅读器
+     * @return <Boolean>
+     */
+    isInstallPDF:function(){
+        var i, len;
+
+        var flag = true;
+
+        if(Nui.browser.webkit || (Nui.browser.mozilla && Nui.browser.version > 19)){
+            flag = false;
+        }
+
+        if(navigator.plugins && (len = navigator.plugins.length)){
+            for(i = 0; i < len; i++){
+                if(/Adobe Reader|Adobe PDF|Acrobat|Chrome PDF Viewer/.test(navigator.plugins[i].name)){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        try{
+            if(window.ActiveXObject || window.ActiveXObject.prototype){
+                for(i = 1; i < 10; i++){
+                    try{
+                        if(eval("new ActiveXObject('PDF.PdfCtrl." + i + "');")){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    catch(e){
+                        flag = true;
+                    }
+                }
+
+                var arr = ['PDF.PdfCtrl', 'AcroPDF.PDF.1', 'FoxitReader.Document', 'Adobe Acrobat', 'Adobe PDF Plug-in'];
+                len = arr.length;
+                for(i = 0; i < len; i++){
+                    try{
+                        if(new ActiveXObject(arr[i])){
+                            flag = false;
+                            break;
+                        }
+
+                    }
+                    catch(e){
+                        flag = true;
+                    }
+                }
+            }
+        }
+        catch(e){}
+
+        return flag;
+    },
+    /**
+     * @func 检测是否需要安装flash，没有安装则返回安装路径
+     * @return <Boolean, String>
+     */
+    isInstallFlash:function(){
+        if(typeof window.ActiveXObject != 'undefined'){
+            try{
+                if(!!new ActiveXObject('ShockwaveFlash.ShockwaveFlash')){
+                    return false
+                }
+            }
+            catch(e){}
+        }
+        else{
+            if(!!navigator.plugins['Shockwave Flash']){
+                return false
+            }
+        }
+        if(Nui.browser.msie){
+            return 'http://rj.baidu.com/soft/detail/17153.html'
+        }
+        else{
+            return 'http://rj.baidu.com/soft/detail/15432.html'
+        }
+    },
+    /**
+     * @func 将数字转换为中文大写
+     * @param money <String> 数字
+     * @return <String>
+     */
+    getUpper:function(money){
+        //汉字的数字
+        var cnNums = new Array('零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖');
+        //基本单位
+        var cnIntRadice = new Array('', '拾', '佰', '仟');
+        //对应整数部分扩展单位
+        var cnIntUnits = new Array('', '万', '亿', '兆');
+        //对应小数部分单位
+        var cnDecUnits = new Array('角', '分', '毫', '厘');
+        //整数金额时后面跟的字符
+        var cnInteger = '整';
+        //整型完以后的单位
+        var cnIntLast = '元';
+        //最大处理的数字
+        var maxNum = 999999999999999.9999;
+        //金额整数部分
+        var integerNum;
+        //金额小数部分
+        var decimalNum;
+        //输出的中文金额字符串
+        var chineseStr = '';
+        //分离金额后用的数组，预定义
+        var parts;
+        if (money == '') { return ''; }
+        money = parseFloat(money);
+        if (money >= maxNum) {
+            //超出最大处理数字
+            return '';
+        }
+        if (money == 0) {
+            chineseStr = cnNums[0] + cnIntLast + cnInteger;
+            return chineseStr;
+        }
+        //转换为字符串
+        money = money.toString();
+        if (money.indexOf('.') == -1) {
+            integerNum = money;
+            decimalNum = '';
+        } else {
+            parts = money.split('.');
+            integerNum = parts[0];
+            decimalNum = parts[1].substr(0, 4);
+        }
+        //获取整型部分转换
+        if (parseInt(integerNum, 10) > 0) {
+            var zeroCount = 0;
+            var IntLen = integerNum.length;
+            for (var i = 0; i < IntLen; i++) {
+            var n = integerNum.substr(i, 1);
+            var p = IntLen - i - 1;
+            var q = p / 4;
+            var m = p % 4;
+            if (n == '0') {
+                zeroCount++;
+            } else {
+                if (zeroCount > 0) {
+                chineseStr += cnNums[0];
+                }
+                //归零
+                zeroCount = 0;
+                chineseStr += cnNums[parseInt(n)] + cnIntRadice[m];
+            }
+            if (m == 0 && zeroCount < 4) {
+                chineseStr += cnIntUnits[q];
+            }
+            }
+            chineseStr += cnIntLast;
+        }
+        //小数部分
+        if (decimalNum != '') {
+            var decLen = decimalNum.length;
+            for (var i = 0; i < decLen; i++) {
+            var n = decimalNum.substr(i, 1);
+            if (n != '0') {
+                chineseStr += cnNums[Number(n)] + cnDecUnits[i];
+            }
+            }
+        }
+        if (chineseStr == '') {
+            chineseStr += cnNums[0] + cnIntLast + cnInteger;
+        } else if (decimalNum == '') {
+            chineseStr += cnInteger;
+        }
+        return chineseStr;
     }
 })
