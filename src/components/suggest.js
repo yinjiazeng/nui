c5eef25f9218a758ed520ce1aab68b2933c7b381
yinjiazeng <yinjiazeng@163.com>
2017-12-24 09:17:45 +0800

    提交

---
+++ src/components/suggest.js
@@ -0,0 +1,27 @@
+/**
+ * @author Aniu[2017-12-23 16:50]
+ * @update Aniu[2017-12-23 16:50]
+ * @version 1.0.1
+ * @description 搜索下拉
+ */
+
+Nui.define(['../core/component'], function(component){
+    return this.extend(component, {
+        _options:{
+            url:'',
+            param
+        },
+        _template:{
+            
+        },
+        _events:{
+            
+        },
+        _init:function(){
+
+        },
+        _exec:function(){
+
+        }
+    })
+}); 
