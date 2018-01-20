8c1bf50a6b7bb6a158381b8f2f7cbcaa1bc914cb
yinjiazeng <yinjiazeng@163.com>
2018-01-20 10:48:21 +0800

    提交

--- pages/components/datagrid/script/demo-min.js
+++ pages/components/datagrid/script/demo-min.js
@@ -5,6 +5,74 @@
         }
         return module
     }
+__define('lib/components/checkradio',function(){
+    
+})
+__define('lib/core/events',function(){
+    return function(opts){
+        var self = this, that = opts || self,
+            constr = that.constructor,
+            isComponent = constr && constr.__component_name,
+            elem = self.element || that.element || Nui.doc, 
+            events = isComponent ? that._events : that.events;
+        if(!elem || !events){
+            return that
+        }
+
+        if(typeof events === 'function'){
+            events = events.call(that)
+        }
+
+        if(!(elem instanceof jQuery)){
+            elem = jQuery(elem)
+        }
+
+        var evt, ele, ret;
+        var callback = function(e, elem, cbs){
+            if(typeof cbs === 'function'){
+                cbs.call(that, e, elem);
+            }
+            else{
+                var _cb, _that;
+                Nui.each(cbs, function(cb, i){
+                    if(typeof (_cb = that[cb]) === 'function'){
+                        _that = that;
+                    }
+                    else if(typeof (_cb = self[cb]) === 'function'){
+                        _that = self;
+                    }
+                    if(_that){
+                        return ret = _cb.call(_that, e, elem, ret);
+                    }
+                })
+            }
+        }
+
+        Nui.each(events, function(cbs, evts){
+            if(cbs && (typeof cbs === 'string' || typeof cbs === 'function')){
+                if(typeof cbs === 'string'){
+                    cbs = Nui.trim(cbs).split(/\s+/);
+                }
+                evts = Nui.trim(evts).split(/\s+/);
+                // keyup:kupdown:focus a => elem.on('keyup kupdown focus', 'a', callback)
+                evt = evts.shift().replace(/:/g, ' ');
+                ele = evts.join(' ');
+                //组件内部处理
+                if(isComponent){
+                    that._on(evt, elem, ele, function(e, elem){
+                        callback(e, elem, cbs)
+                    })
+                }
+                else{
+                    elem.on(evt, ele, function(e){
+                        callback(e, jQuery(this), cbs)
+                    })
+                }
+            }
+        })
+        return that
+    }
+})
 /**
  * @author Aniu[2016-11-11 16:54]
  * @update Aniu[2016-11-11 16:54]
@@ -568,71 +636,6 @@ __define('lib/core/util',{
     }
 })
 
-__define('lib/core/events',function(){
-    return function(opts){
-        var self = this, that = opts || self,
-            constr = that.constructor,
-            isComponent = constr && constr.__component_name,
-            elem = self.element || that.element || Nui.doc, 
-            events = isComponent ? that._events : that.events;
-        if(!elem || !events){
-            return that
-        }
-
-        if(typeof events === 'function'){
-            events = events.call(that)
-        }
-
-        if(!(elem instanceof jQuery)){
-            elem = jQuery(elem)
-        }
-
-        var evt, ele, ret;
-        var callback = function(e, elem, cbs){
-            if(typeof cbs === 'function'){
-                cbs.call(that, e, elem);
-            }
-            else{
-                var _cb, _that;
-                Nui.each(cbs, function(cb, i){
-                    if(typeof (_cb = that[cb]) === 'function'){
-                        _that = that;
-                    }
-                    else if(typeof (_cb = self[cb]) === 'function'){
-                        _that = self;
-                    }
-                    if(_that){
-                        return ret = _cb.call(_that, e, elem, ret);
-                    }
-                })
-            }
-        }
-
-        Nui.each(events, function(cbs, evts){
-            if(cbs && (typeof cbs === 'string' || typeof cbs === 'function')){
-                if(typeof cbs === 'string'){
-                    cbs = Nui.trim(cbs).split(/\s+/);
-                }
-                evts = Nui.trim(evts).split(/\s+/);
-                // keyup:kupdown:focus a => elem.on('keyup kupdown focus', 'a', callback)
-                evt = evts.shift().replace(/:/g, ' ');
-                ele = evts.join(' ');
-                //组件内部处理
-                if(isComponent){
-                    that._on(evt, elem, ele, function(e, elem){
-                        callback(e, elem, cbs)
-                    })
-                }
-                else{
-                    elem.on(evt, ele, function(e){
-                        callback(e, jQuery(this), cbs)
-                    })
-                }
-            }
-        })
-        return that
-    }
-})
 /**
  * @author Aniu[2016-11-11 16:54]
  * @update Aniu[2016-01-12 04:33]
@@ -1405,10 +1408,15 @@ __define('lib/core/component',['lib/core/template', 'lib/core/events'], function
     })
 })
 
-__define('lib/components/datagrid',function(){
+__define('lib/components/paging',['lib/core/component'], function(component){
+    
+})
+__define('lib/components/datagrid',function(require){
     var module = this;
-    var component = module.require('lib/core/component');
-    var util = module.require('lib/core/util');
+    var component = require('lib/core/component');
+    var util = require('lib/core/util');
+    var paging = require('lib/components/paging');
+    var checkradio = require('lib/components/checkradio');
 
     var scrollBarWidth = (function(){
         var oldWidth, newWidth, div = document.createElement('div');
@@ -1480,12 +1488,10 @@ __define('lib/components/datagrid',function(){
             isActive:true,
             isBorder:true,
             option:null,
-            //��ʼ��ʱ�Ƿ���÷�ҳ
             isPaging:true,
             isDir:false,
             keyCode:[9, 13],
             url:null,
-            //��ҳ����
             paging:null,
             fields:null,
             dataField:'list',
