ad9a8bf7ed9f89886a042caf3562c9fd8f630cb5
yinjiazeng <yinjiazeng@163.com>
2018-01-26 10:23:47 +0800

    提交

--- config.js
+++ config.js
@@ -22,6 +22,6 @@ Nui.config({
         "./script/template/page": "731c371",
         "./script/demo": "eeab945",
         "./page": "c7c6af7",
-        "./script/page": "af9453f"
+        "./script/page": "0e8cd30"
     }
 })
