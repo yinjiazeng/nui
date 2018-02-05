de07b3d4019223628fddd897e92f09d520df1de9
yinjiazeng <yinjiazeng@163.com>
2018-02-05 20:01:02 +0800

    提交文档

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ Nui.config({
         "{script}/base": "e841e48",
         "./script/events/page": "346ee7e",
         "./script/template/page": "7da3c86",
-        "./script/demo": "455231c",
-        "./script/page": "eb12d2d"
+        "./script/page": "eb12d2d",
+        "./script/demo": "455231c"
     }
 })
